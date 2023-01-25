bbeb7d9ddd59fdf07d248223a88033418b1edd24
atemcozz <atemcozzartem@gmail.com>
2023-01-25 20:31:44 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const peer_id = document.querySelector("#peerid");
+const message = document.querySelector("#message");
+const button = document.querySelector("#send");
+
+button.addEventListener("click", () => {
+  fetch("https://lev-bot.adaptable.app/message", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      message: message.value,
+      peer_id: peer_id.value,
+    }),
+  }).then(() => {
+    message.value = "";
+  });
+});
